4a6d85b340bf6236947996db5b0d3c2a550f0150
styy <satyam99jha@gmail.com>
2024-03-19 12:18:27 +0530

    minify

--- scripts/avsec.js
+++ scripts/avsec.js
@@ -1,34 +1,3 @@
-function allCheck() {
-if (localStorage.getItem('token') === null) {
-    window.location.replace("/identity/");
-} else {}}
-const avatars = document.querySelectorAll('.avatarsList img');
-/*const upload = document.querySelector('.custom');
-upload.addEventListener('click', () => {
-    document.getElementById('image').click();
-})    */
-avatars.forEach((avatar) => {
-    avatar.addEventListener('click', () => {
-        var uid = localStorage.getItem('uid');
-        var token = localStorage.getItem('token');
-        const data = {
-            avatar: "https://chat.styy.me/avatars/defaults/" + event.target.src.split('/').pop(),
-            avuid: uid,
-            avtoken: token
-        };
-        const payload = {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(data)
-        }
-        fetch('https://api.styy.me/v1/avatar/update', payload)
-        .then(response => response.text())
-        .then(data => {
-            if (data === "ok") {
-                window.location.replace("/app/");
-            } else {
-                alert("smth went wrong whdafack!")
-            }})
-        .catch(error => {console.error('Error:', error);});
-    });
-});
+function allCheck(){if(localStorage.getItem('token')===null){window.location.replace("/identity/")}else{}}
+const avatars=document.querySelectorAll('.avatarsList img');avatars.forEach((avatar)=>{avatar.addEventListener('click',()=>{var uid=localStorage.getItem('uid');var token=localStorage.getItem('token');const data={avatar:"https://chat.styy.me/avatars/defaults/"+event.target.src.split('/').pop(),avuid:uid,avtoken:token};const payload={method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(data)}
+fetch('https://api.styy.me/v1/avatar/update',payload).then(response=>response.text()).then(data=>{if(data==="ok"){window.location.replace("/app/")}else{alert("smth went wrong whdafack!")}}).catch(error=>{console.error('Error:',error)})})})
--- scripts/login.js
+++ scripts/login.js
@@ -1,53 +1,6 @@
-function allCheck() {
-if (localStorage.getItem('token')) {
-    fetch("https://api.styy.me/v1/check/data", {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            uid: localStorage.getItem('uid'),
-            check: "avatar"
-        })
-    }).then(response => response.text()).then(data => {
-        if (data != "yep") {
-            window.location.replace("/avatars/");
-        } else {
-            window.location.replace("/app/");
-        }
-    })
-} else {}}    
-const createBtn = document.querySelector('span');
-var userid = document.getElementById('username');
-var pass = document.getElementById('pass');
-
-function changeUi() {
-    createBtn.innerHTML = '<i class="fa fa-circle-o-notch fa-spin"></i>';
-}
-function dataApi() {
-    if (userid.value === '' || pass.value === '') {
-        alert('Username and password cannot be empty.');
-        location.reload(); // Reload the page
-        return; // Exit the function
-    }
-    const data = {
-        username: userid.value,
-        password: pass.value
-    };
-    const payload = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    }
-    fetch('https://api.styy.me/v1/create', payload)
-    .then(response => response.text())
-    .then(data => {
-            localStorage.setItem('token', JSON.parse(data).token);
-            localStorage.setItem('uid', JSON.parse(data).uid);
-            window.location.replace("/avatars/")
-    })
-    .catch(error => {console.error('Error:', error);});
-}
-
-createBtn.addEventListener('click', () => {
-    changeUi();
-    dataApi();
-});
+function allCheck(){if(localStorage.getItem('token')){fetch("https://api.styy.me/v1/check/data",{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({uid:localStorage.getItem('uid'),check:"avatar"})}).then(response=>response.text()).then(data=>{if(data!="yep"){window.location.replace("/avatars/")}else{window.location.replace("/app/")}})}else{}}
+const createBtn=document.querySelector('span');var userid=document.getElementById('username');var pass=document.getElementById('pass');function changeUi(){createBtn.innerHTML='<i class="fa fa-circle-o-notch fa-spin"></i>'}
+function dataApi(){if(userid.value===''||pass.value===''){alert('Username and password cannot be empty.');location.reload();return}
+const data={username:userid.value,password:pass.value};const payload={method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(data)}
+fetch('https://api.styy.me/v1/create',payload).then(response=>response.text()).then(data=>{localStorage.setItem('token',JSON.parse(data).token);localStorage.setItem('uid',JSON.parse(data).uid);window.location.replace("/avatars/")}).catch(error=>{console.error('Error:',error)})}
+createBtn.addEventListener('click',()=>{changeUi();dataApi()})
